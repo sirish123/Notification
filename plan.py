d4d4957d2590d192dcb9cd939bccd6f84395a239
Sirish Sekhar <sirish1527@gmail.com>
2021-08-03 20:30:34 +0530

    Update plan.py

--- plan.py
+++ plan.py
@@ -23,14 +23,64 @@ if __name__ == "__main__":
         now = datetime.now(tz_NY) 
         current_time = now.strftime("%H:%M:%S")
         #info on time and convert to IST----
-        if workingday==listdays[1]:
-            if current_time>"15:53:50" and current_time<"15:54:01":
+        if workingday==listdays[0]:
+            if current_time>"8:59:50" and current_time<"9:00:00":
                 count=0;
                 while(count!=1):
                     time.sleep(5)
                     count+=1
                     notification.notify(
                     title = "TIME TABLE!",
-                    message =  "BEE"+"\n SWAPNIL SIR",
+                    message =  "BEE"+" SWAPNIL SIR",
+                    app_icon = "calendar_clock_schedule_icon-icons.com_51085.ico",
+                )
+            if current_time>"9:59:50" and current_time<"10:00:00":
+                count=0;
+                while(count!=1):
+                    time.sleep(5)
+                    count+=1
+                    notification.notify(
+                    title = "TIME TABLE!",
+                    message =  "SOM"+" VENKAT RAMAN P",
+                    app_icon = "calendar_clock_schedule_icon-icons.com_51085.ico",
+                )
+            if current_time>"10:59:50" and current_time<"11:00:00":
+                count=0;
+                while(count!=1):
+                    time.sleep(5)
+                    count+=1
+                    notification.notify(
+                    title = "TIME TABLE!",
+                    message =  "Thermodynamics"+"ANIL KUMAR E",
+                    app_icon = "calendar_clock_schedule_icon-icons.com_51085.ico",
+                )
+            if current_time>"11:59:50" and current_time<"12:00:00":
+                count=0;
+                while(count!=1):
+                    time.sleep(5)
+                    count+=1
+                    notification.notify(
+                    title = "TIME TABLE!",
+                    message =  "BEE"+" SWAPNIL SIR",
+                    app_icon = "calendar_clock_schedule_icon-icons.com_51085.ico",
+                )
+            if current_time>"9:59:50" and current_time<"10:00:00":
+                count=0;
+                while(count!=1):
+                    time.sleep(5)
+                    count+=1
+                    notification.notify(
+                    title = "TIME TABLE!",
+                    message =  "BEE"+" SWAPNIL SIR",
+                    app_icon = "calendar_clock_schedule_icon-icons.com_51085.ico",
+                )
+            if current_time>"9:59:50" and current_time<"10:00:00":
+                count=0;
+                while(count!=1):
+                    time.sleep(5)
+                    count+=1
+                    notification.notify(
+                    title = "TIME TABLE!",
+                    message =  "BEE"+" SWAPNIL SIR",
                     app_icon = "calendar_clock_schedule_icon-icons.com_51085.ico",
                 )
